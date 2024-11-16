cbed882ba38863bc010b5a84149157eed2ae1789
zhangqizong6 <1012424837@qq.com>
2024-11-16 22:56:22 +0800

    zhangqz: PhotoUtil.java push

---
+++ src/test/java/com/austin/javautils/JavaUtilsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.austin.javautils;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaUtilsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
